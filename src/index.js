f4944c86c1f8b34b1c1007de0f3118a28cdee37f
yolo <kuroikenshi@sina.com>
2018-03-23 09:32:04 +0800

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import _ from 'lodash';
+import './style.css';
+import './style2.less';
+import Icon from './icon.png';
 
 function component() {
 	var element = document.createElement('div');
 
-	element.innerHTML = _.join(['Hello', 'Webpack', 'CHANGE'], ' ');
+	element.innerHTML = _.join(['Hello', 'Webpack', 'zzz'], ' ');
+	element.classList.add('hello');
+
+	var myIcon = new Image();
+	myIcon.src = Icon;
+
+	element.appendChild(myIcon);
 
 	return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,36 @@ module.exports = {
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist')
+	},
+	// watch: true,
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.less$/,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 1
+						}
+					},
+					'less-loader'
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader'
+				]
+			}
+		]
 	}
 };
